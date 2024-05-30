dc29108cc5503b360d70ff2571e3592fc3291975
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-30 16:14:25 +0200

    implement run and open command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello, world!");
+use std::process;
+
+use clap::{Arg, ArgAction, Command};
+
+use self::url::PumlUrlCreator;
+
+mod url;
+
+fn main() -> eyre::Result<()> {
+    let cli = Command::new("puml")
+        .subcommands([
+            Command::new("run").about("run the docker plantuml server"),
+            Command::new("open")
+                .args([
+                    Arg::new("file")
+                        .required(true)
+                        .num_args(1)
+                        .help("the file to open"),
+                    Arg::new("pdf")
+                        .long("pdf")
+                        .action(ArgAction::SetTrue)
+                        .help("open as pdf"),
+                    Arg::new("png")
+                        .long("png")
+                        .action(ArgAction::SetTrue)
+                        .help("open as png"),
+                    Arg::new("ascii")
+                        .long("ascii")
+                        .action(ArgAction::SetTrue)
+                        .help("open as ascii"),
+                ])
+                .about("open a file in the browser"),
+        ])
+        .subcommand_required(true)
+        .about("a plantuml cli providing a few extra scripts")
+        .get_matches();
+
+    match cli.subcommand().unwrap() {
+        ("run", _) => {
+            let command = process::Command::new("docker")
+                .args([
+                    "run",
+                    "-d",
+                    "-p",
+                    "8080:8080",
+                    "plantuml/plantuml-server:jetty",
+                ])
+                .status()?;
+            println!("{}", command);
+        }
+        ("open", command) => {
+            let file: &String = command.get_one("file").unwrap();
+
+            let url_creator = PumlUrlCreator::new()?;
+
+            let url = if command.get_flag("png") {
+                url_creator.create_png_url(file)
+            } else if command.get_flag("pdf") {
+                url_creator.create_pdf_url(file)
+            } else if command.get_flag("ascii") {
+                url_creator.create_ascii_url(file)
+            } else {
+                url_creator.create_svg_url(file)
+            }?;
+
+            const FIREFOX: &str = "C:\\'Program Files'\\'Mozilla Firefox'\\firefox.exe";
+            let command = format!("\"{} -new-tab \\\"{}\\\"\"", FIREFOX, url);
+
+            _ = process::Command::new("powershell.exe")
+                .args(["-Command", &command])
+                .status()?;
+
+            println!("{}", url);
+        }
+        _ => {}
+    };
+
+    Ok(())
 }
---
+++ src/url.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use std::path::Path;
+
+use base64::alphabet::Alphabet;
+use base64::engine::{GeneralPurpose, GeneralPurposeConfig};
+use base64::Engine;
+use miniz_oxide::deflate::compress_to_vec_zlib;
+
+pub struct PumlUrlCreator {
+    b64_engine: GeneralPurpose,
+}
+
+enum UrlType {
+    Png,
+    Svg,
+    Pdf,
+    Ascii,
+}
+
+impl PumlUrlCreator {
+    pub fn new() -> eyre::Result<Self> {
+        let alphabet =
+            Alphabet::new("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_")?;
+        let config = GeneralPurposeConfig::new().with_encode_padding(false);
+        let engine = GeneralPurpose::new(&alphabet, config);
+        Ok(Self { b64_engine: engine })
+    }
+
+    pub fn create_png_url(&self, path: impl AsRef<Path>) -> eyre::Result<String> {
+        self.create_url(path, UrlType::Png)
+    }
+
+    pub fn create_svg_url(&self, path: impl AsRef<Path>) -> eyre::Result<String> {
+        self.create_url(path, UrlType::Svg)
+    }
+
+    pub fn create_pdf_url(&self, path: impl AsRef<Path>) -> eyre::Result<String> {
+        self.create_url(path, UrlType::Pdf)
+    }
+
+    pub fn create_ascii_url(&self, path: impl AsRef<Path>) -> eyre::Result<String> {
+        self.create_url(path, UrlType::Ascii)
+    }
+
+    fn create_url(&self, path: impl AsRef<Path>, url_type: UrlType) -> eyre::Result<String> {
+        let url_type = url_type.as_str();
+        let payload = self.encode_file(path)?;
+        Ok(format!("https://localhost:8080/{url_type}/{payload}"))
+    }
+
+    fn encode_file(&self, path: impl AsRef<Path>) -> eyre::Result<String> {
+        let content = read_to_string(path)?;
+        let content = content.trim();
+        let content = content.strip_prefix("@startuml").unwrap_or(content);
+        let content = content.strip_suffix("@enduml").unwrap_or(content);
+        let content = content.trim();
+        Ok(self.encode(content))
+    }
+
+    fn encode(&self, text: &str) -> String {
+        let compressed = compress_to_vec_zlib(text.as_bytes(), 10);
+        self.b64_engine
+            .encode(&compressed[2..(compressed.len() - 4)])
+    }
+}
+
+impl UrlType {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Self::Png => "png",
+            Self::Svg => "svg",
+            Self::Pdf => "pdf",
+            Self::Ascii => "ascii",
+        }
+    }
+}
